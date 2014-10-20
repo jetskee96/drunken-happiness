0dfcf17e05f1c5addd86879a2e2d8535201d6ae8
jetskee96 <jetskee96@yahoo.com>
2014-10-20 22:46:43 +0000

    images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141020163851) do
+ActiveRecord::Schema.define(version: 20141020224509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20141020163851) do
     t.string   "theme"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
